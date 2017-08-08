a19148f5a2a5ee59075befe118c746773da39bda
George Hahn <george.hahn.vhs@gmail.com>
2017-08-08 12:31:08 -0500

    Add debug tracing, format file

--- src/main.c
+++ src/main.c
@@ -1,56 +1,60 @@
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
 
-
-#include "rbc_mesh.h"
 #include "mesh_aci.h"
+#include "rbc_mesh.h"
 
-#include "softdevice_handler.h"
-#include "app_error.h"
-#include "nrf_gpio.h"
-#include "boards.h"
-#include "leds.h"
-#include "pstorage_platform.h"
 #include "app_cmd.h"
-#include "config.h"
-#include "sensor.h"
-#include "transport_control.h"
-#include "serial_handler.h"
-#include "scheduler.h"
-#include "heartbeat.h"
-#include "handles.h"
+#include "app_error.h"
+#include "boards.h"
 #include "bsp.h"
+#include "config.h"
+#include "handles.h"
+#include "heartbeat.h"
+#include "leds.h"
 #include "mesh_control.h"
+#include "nrf_gpio.h"
+#include "pstorage_platform.h"
+#include "scheduler.h"
+#include "sensor.h"
+#include "serial_handler.h"
+#include "softdevice_handler.h"
+#include "transport_control.h"
 #include <stdbool.h>
 #include <stdint.h>
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
+
+#define NRF_LOG_MODULE_NAME "main.c"
 
 #if NORDIC_SDK_VERSION >= 11
 #include "nrf_nvic.h"
 #endif
 
-#define MESH_ACCESS_ADDR        (0xA555410C)
-#define MESH_INTERVAL_MIN_MS    (100)
+#define MESH_ACCESS_ADDR (0xA555410C)
+#define MESH_INTERVAL_MIN_MS (100)
 
 #if NORDIC_SDK_VERSION >= 11
 nrf_nvic_state_t nrf_nvic_state = {0};
-static nrf_clock_lf_cfg_t m_clock_cfg = 
-{
-    .source = NRF_CLOCK_LF_SRC_XTAL,    
-    .xtal_accuracy = NRF_CLOCK_LF_XTAL_ACCURACY_75_PPM
-};
+static nrf_clock_lf_cfg_t m_clock_cfg = {.source = NRF_CLOCK_LF_SRC_XTAL,
+                                         .xtal_accuracy =
+                                             NRF_CLOCK_LF_XTAL_ACCURACY_75_PPM};
 
-#define MESH_CLOCK_SOURCE       (m_clock_cfg)    /**< Clock source used by the Softdevice. For calibrating timeslot time. */
+#define MESH_CLOCK_SOURCE                                                      \
+  (m_clock_cfg) /**< Clock source used by the Softdevice. For calibrating      \
+                   timeslot time. */
 #else
-#define MESH_CLOCK_SOURCE       (NRF_CLOCK_LFCLKSRC_XTAL_75_PPM)    /**< Clock source used by the Softdevice. For calibrating timeslot time. */
+#define MESH_CLOCK_SOURCE                                                      \
+  (NRF_CLOCK_LFCLKSRC_XTAL_75_PPM) /**< Clock source used by the               \
+                                      Softdevice.For calibrating timeslot      \
+                                      time. */
 #endif
 
 /** @brief General error handler. */
-static inline void error_loop(void)
-{
+static inline void error_loop(void) {
   TOGGLE_LED(LED_RED);
   __disable_irq();
-  while (true)
-  {
+  while (true) {
     __WFE();
   }
 }
@@ -62,14 +66,15 @@ static inline void error_loop(void)
 * @param[in] line_num Line where the error check failed
 * @param[in] p_file_name File where the error check failed
 */
-void sd_assert_handler(uint32_t pc, uint16_t line_num, const uint8_t* p_file_name)
-{
+void sd_assert_handler(uint32_t pc, uint16_t line_num,
+                       const uint8_t *p_file_name) {
+  NRF_LOG_ERROR("sd_assert_handler (looping forever)");
   error_loop();
 }
 
 /** @brief Hardware fault handler. */
-void HardFault_Handler(void)
-{
+void HardFault_Handler(void) {
+  NRF_LOG_ERROR("HardFault_Handler (looping forever)");
   error_loop();
 }
 
@@ -78,64 +83,62 @@ void HardFault_Handler(void)
 *
 * @param[in] p_evt Mesh event propagated from framework.
 */
-static void rbc_mesh_event_handler(rbc_mesh_event_t* p_evt)
-{
+static void rbc_mesh_event_handler(rbc_mesh_event_t *p_evt) {
 
-  switch (p_evt->type)
-  {
-    case RBC_MESH_EVENT_TYPE_CONFLICTING_VAL:
-    case RBC_MESH_EVENT_TYPE_NEW_VAL:
-    case RBC_MESH_EVENT_TYPE_UPDATE_VAL:
-      if (p_evt->params.rx.value_handle == MESH_CONTROL_HANDLE && p_evt->params.rx.data_len == sizeof(mesh_control_t)) {
-        mesh_control_update_config((mesh_control_t*)p_evt->params.rx.p_data);
-      }
-      break;
-    case RBC_MESH_EVENT_TYPE_TX:
-    case RBC_MESH_EVENT_TYPE_INITIALIZED:
-    case RBC_MESH_EVENT_TYPE_DFU_NEW_FW_AVAILABLE:
-    case RBC_MESH_EVENT_TYPE_DFU_RELAY_REQ:
-    case RBC_MESH_EVENT_TYPE_DFU_SOURCE_REQ:
-    case RBC_MESH_EVENT_TYPE_DFU_START:
-    case RBC_MESH_EVENT_TYPE_DFU_END:
-    case RBC_MESH_EVENT_TYPE_DFU_BANK_AVAILABLE:
-      break;
+  switch (p_evt->type) {
+  case RBC_MESH_EVENT_TYPE_CONFLICTING_VAL:
+  case RBC_MESH_EVENT_TYPE_NEW_VAL:
+  case RBC_MESH_EVENT_TYPE_UPDATE_VAL:
+    if (p_evt->params.rx.value_handle == MESH_CONTROL_HANDLE &&
+        p_evt->params.rx.data_len == sizeof(mesh_control_t)) {
+      mesh_control_update_config((mesh_control_t *)p_evt->params.rx.p_data);
+    }
+    break;
+  case RBC_MESH_EVENT_TYPE_TX:
+  case RBC_MESH_EVENT_TYPE_INITIALIZED:
+  case RBC_MESH_EVENT_TYPE_DFU_NEW_FW_AVAILABLE:
+  case RBC_MESH_EVENT_TYPE_DFU_RELAY_REQ:
+  case RBC_MESH_EVENT_TYPE_DFU_SOURCE_REQ:
+  case RBC_MESH_EVENT_TYPE_DFU_START:
+  case RBC_MESH_EVENT_TYPE_DFU_END:
+  case RBC_MESH_EVENT_TYPE_DFU_BANK_AVAILABLE:
+    break;
   }
 }
 
 /* dispatch system events to interested modules. */
-static void sys_evt_dispatch(uint32_t sys_evt)
-{
-    //pstorage_sys_event_handler(sys_evt);
-    //ble_advertising_on_sys_evt(sys_evt);
+static void sys_evt_dispatch(uint32_t sys_evt) {
+  // pstorage_sys_event_handler(sys_evt);
+  // ble_advertising_on_sys_evt(sys_evt);
 }
 
+void clock_initialization() {
+  NRF_LOG_DEBUG("Initialize: clock");
+  NRF_CLOCK->LFCLKSRC = (CLOCK_LFCLKSRC_SRC_Xtal << CLOCK_LFCLKSRC_SRC_Pos);
+  NRF_CLOCK->EVENTS_LFCLKSTARTED = 0;
+  NRF_CLOCK->TASKS_LFCLKSTART = 1;
 
-void clock_initialization()
-{
-    NRF_CLOCK->LFCLKSRC            = (CLOCK_LFCLKSRC_SRC_Xtal << CLOCK_LFCLKSRC_SRC_Pos);
-    NRF_CLOCK->EVENTS_LFCLKSTARTED = 0;
-    NRF_CLOCK->TASKS_LFCLKSTART    = 1;
+  while (NRF_CLOCK->EVENTS_LFCLKSTARTED == 0) {
+    // Do nothing.
+  }
 
-    while (NRF_CLOCK->EVENTS_LFCLKSTARTED == 0)
-    {
-        // Do nothing.
-    }
-
-    NRF_CLOCK->EVENTS_LFCLKSTARTED = 0;
+  NRF_CLOCK->EVENTS_LFCLKSTARTED = 0;
 }
 
 static void packet_peek_cb(rbc_mesh_packet_peek_params_t *params) {
   if (params->packet_type == BLE_PACKET_TYPE_ADV_NONCONN_IND &&
       params->p_payload[1] == HEARTBEAT_ADV_DATA_TYPE) {
-    received_heartbeat((heartbeat_ad_t*)&params->p_payload[2], params->rssi);
+    received_heartbeat((heartbeat_ad_t *)&params->p_payload[2], params->rssi);
   }
 }
 
-int main(void)
-{
+int main(void) {
+  NRF_LOG_INIT(NULL);
 
+  NRF_LOG_DEBUG("Initialize: bsp");
   bsp_init(BSP_INIT_BUTTONS & BSP_INIT_LED, 0, 0);
 
+  NRF_LOG_DEBUG("Initialize: mesh_control");
   mesh_control_init();
 
 #if LEDS_NUMBER > 0
@@ -144,30 +147,34 @@ int main(void)
   nrf_gpio_cfg_output(LED_START + LED_BLUE);
 #endif
 
+  NRF_LOG_DEBUG("Initialize: SoftDevice");
   /* Enable Softdevice (including sd_ble before framework */
   SOFTDEVICE_HANDLER_INIT(&MESH_CLOCK_SOURCE, NULL);
   softdevice_ble_evt_handler_set(rbc_mesh_ble_evt_handler);
 
-  //clock_initialization();
-  //sd_softdevice_disable();
+  // clock_initialization();
+  // sd_softdevice_disable();
 
   // Register with the SoftDevice handler module for system events.
   softdevice_sys_evt_handler_set(sys_evt_dispatch);
 
-  // Debug pins
-  // nrf_gpio_cfg_output(5);
-  // nrf_gpio_cfg_output(6);
+// Debug pins
+// nrf_gpio_cfg_output(5);
+// nrf_gpio_cfg_output(6);
 
-  // Disable simblee's proximityMode
+// Disable simblee's proximityMode
 #ifdef SIMBLEE
+  NRF_LOG_DEBUG("Initialize: Simblee radio");
   nrf_gpio_cfg_output(31);
   CLEAR_PIN(31);
 #endif
 
-  // if (NRF_CLOCK->LFCLKSRC == (CLOCK_LFCLKSRC_SRC_Xtal << CLOCK_LFCLKSRC_SRC_Pos)) {
+  // if (NRF_CLOCK->LFCLKSRC == (CLOCK_LFCLKSRC_SRC_Xtal <<
+  // CLOCK_LFCLKSRC_SRC_Pos)) {
   //   TOGGLE_LED(LED_GREEN);
   // }
 
+  NRF_LOG_DEBUG("Initialize: mesh");
   /* Initialize mesh. */
   rbc_mesh_init_params_t init_params;
   init_params.access_addr = MESH_ACCESS_ADDR;
@@ -179,8 +186,7 @@ int main(void)
   uint32_t error_code;
   error_code = rbc_mesh_init(init_params);
   APP_ERROR_CHECK(error_code);
-  //led_config(LED_GREEN, 1);
-
+  // led_config(LED_GREEN, 1);
 
   // This has to come after rbc_mesh_init for some reason.  Otherwise we
   // get a HardFault when rbc_mesh_init is called
@@ -196,8 +202,11 @@ int main(void)
   //   tc_radio_params_set(MESH_ACCESS_ADDR, app_config.mesh_channel);
   // }
 
-  scheduler_init(app_config.sleep_enabled); // Initializes, but does not start, clock
-  heartbeat_init(DEFAULT_MESH_CHANNEL); // Inits structures for sending heartbeat
+  NRF_LOG_DEBUG("Initialize: heartbeat");
+  scheduler_init(
+      app_config.sleep_enabled); // Initializes, but does not start, clock
+  heartbeat_init(
+      DEFAULT_MESH_CHANNEL); // Inits structures for sending heartbeat
 
   /* Initialize mesh ACI */
   mesh_aci_init();
@@ -205,6 +214,7 @@ int main(void)
 
   // Stop serial if serial is disabled
   if (!app_config.serial_enabled) {
+    NRF_LOG_DEBUG("Disabling serial output");
     serial_handler_stop();
   }
 
@@ -222,9 +232,9 @@ int main(void)
   // Start clock
   start_clock(0);
 
-   //rbc_mesh_stop();
-  //rbc_mesh_start();
-  //sd_app_evt_wait();
+  // rbc_mesh_stop();
+  // rbc_mesh_start();
+  // sd_app_evt_wait();
   // while(1) {
   //   __WFE();
   //   __SEV();
