6c7198030b830b61b35cb98bcff910e44a460c0b
George Hahn <george.hahn.vhs@gmail.com>
2017-08-30 13:31:29 -0500

    Don't drop config silently in debug mode + autoformat

--- src/mesh_control.c
+++ src/mesh_control.c
@@ -1,7 +1,7 @@
 
 #include "mesh_control.h"
-#include "transport_control.h"
 #include "leds.h"
+#include "transport_control.h"
 
 static mesh_control_t m_config;
 
@@ -11,30 +11,27 @@ void mesh_control_init() {
   m_config.enable_ble = 0;
 }
 
-uint16_t mesh_control_get_wake_interval() {
-  return m_config.wake_interval;
-}
+uint16_t mesh_control_get_wake_interval() { return m_config.wake_interval; }
 
-uint8_t mesh_control_get_hb_tx_power() {
-  return m_config.hb_tx_power;
-}
+uint8_t mesh_control_get_hb_tx_power() { return m_config.hb_tx_power; }
 
 void mesh_control_update_config(mesh_control_t *new_config) {
   rbc_mesh_txpower_t allowed_tx_power[] = {
-    RBC_MESH_TXPOWER_0dBm,
-    RBC_MESH_TXPOWER_Pos4dBm,
-    RBC_MESH_TXPOWER_Neg12dBm,
-    RBC_MESH_TXPOWER_Neg8dBm,
-    RBC_MESH_TXPOWER_Neg4dBm
-  };
+      RBC_MESH_TXPOWER_0dBm, RBC_MESH_TXPOWER_Pos4dBm,
+      RBC_MESH_TXPOWER_Neg12dBm, RBC_MESH_TXPOWER_Neg8dBm,
+      RBC_MESH_TXPOWER_Neg4dBm};
 
   // Sanity checks
   if (new_config->wake_interval == 0 || new_config->wake_interval > 1000) {
+    log("Bad config: new_config->wake_interval=%d out of acceptable range "
+        "(1-1000)",
+        new_config->wake_interval);
     return;
   }
 
   bool tx_allowed = false;
-  for (int i=0; i<sizeof(allowed_tx_power)/sizeof(rbc_mesh_txpower_t); i++) {
+  for (int i = 0; i < sizeof(allowed_tx_power) / sizeof(rbc_mesh_txpower_t);
+       i++) {
     if (allowed_tx_power[i] == new_config->hb_tx_power) {
       tx_allowed = true;
     }
